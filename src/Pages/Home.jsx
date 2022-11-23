e3f1b0197f8e7ccc6fe60a9f556c56548769ab68
Pratik-Kumar-621 <70286186+Pratik-Kumar-621@users.noreply.github.com>
2022-11-23 21:12:03 +0530

    final Desktop

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import About from "../Section/About/About";
 import Features from "../Section/Features/Features";
+import Footer from "../Section/Footer/Footer";
 import Landing from "../Section/Landing/Landing";
 import Services from "../Section/Services/Services";
 import Swapping from "../Section/Swapping/Swapping";
@@ -13,6 +14,7 @@ const Home = () => {
       <Features />
       <Services />
       <Swapping />
+      <Footer />
     </div>
   );
 };
---
+++ src/Section/Footer/Footer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Heading from "../../Media/Landing/Heading.png";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import EmailIcon from "@mui/icons-material/Email";
+import Button from "@mui/material/Button";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="logo-foot">
+        <img src={Heading} alt="Heading" />
+        <div className="foot-name">Electrica Energy</div>
+        <div className="foot-desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </div>
+      </div>
+      <div className="address-foot">
+        <div className="foot-title">Address</div>
+        <div className="foot-desc">
+          <LocationOnIcon />
+          &nbsp; 123, ABC Street, XYZ City, India - 123456
+        </div>
+      </div>
+      <div className="explore-foot">
+        <div className="foot-title">Explore</div>
+        <div className="links-foot">
+          <Button>
+            <LinkedInIcon />
+          </Button>
+          <Button>
+            <TwitterIcon />
+          </Button>
+          <Button>
+            <EmailIcon />
+          </Button>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
